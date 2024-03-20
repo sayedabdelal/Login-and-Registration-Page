8db504ee68cd5bd8783fb511dd0ad3a1ad038eb6
sayed <saidmo19734@gmail.com>
2024-03-20 21:14:10 +0100

    commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Login-and-Register-Page
+
+This project is a simple login and registration page
+
+## Demo
+
+You can try out a live demo of this project [here](https://sayedabdelal.github.io/Login-and-Rigster-Page/).
+
+# Login-and-Registration-Page
---
+++ script.js
@@ -0,0 +1,31 @@
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+const btnPopup = document.querySelector(".btnLogin-popup");
+const iconClose = document.querySelector(".icon-close");
+
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("active");
+});
+
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+});
+
+btnPopup.addEventListener("click", () => {
+  wrapper.classList.add("active-popup");
+});
+
+iconClose.addEventListener("click", () => {
+  wrapper.classList.remove("active-popup");
+});
+
+
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const nav = document.querySelector(".nav");
+
+hamburgerMenu.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  console.log("active");
+});
+
